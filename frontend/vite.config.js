609b8eb6b9a38389cff608e56a69e02d8acff2da
nadavbrz <nadavbrz@gmail.com>
2024-10-29 16:41:24 +0200

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Listen on all interfaces
+    port: 5173, // Port to use
+  },
+});
